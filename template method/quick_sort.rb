c5f5a034e4c180a8471ee901a67372331a7acfad
tanduy <tanduy.vo@gmail.com>
2010-03-24 16:45:59 +0900

    correct code style

--- template method/quick_sort.rb
+++ template method/quick_sort.rb
@@ -1,13 +1,15 @@
 class QuickSort < Sort
+
   def algorithm
     quicksort(a, 0, a.length-1)
   end
-  
+
   def quicksort(list, p, r)
     if p < r then
-        q = partition(list, p, r)
-        quicksort(list, p, q-1)
-        quicksort(list, q+1, r)
+      q = partition(list, p, r)
+      puts("#{list}")
+      quicksort(list, p, q-1)
+      quicksort(list, q+1, r)
     end
   end
 
@@ -15,10 +17,10 @@ class QuickSort < Sort
     pivot = list[r]
     i = p - 1
     p.upto(r-1) do |j|
-        if list[j] <= pivot
-            i = i+1
-            list[i], list[j] = list[j],list[i]
-        end
+      if list[j] <= pivot
+        i = i+1
+        list[i], list[j] = list[j],list[i]
+      end
     end
     list[i+1],list[r] = list[r],list[i+1]
     return i + 1
